9e1f65fee72dc35fb27ac7ad1adc3db7da6e222d
CSUMII\csumii <csumii1010@gmail.com>
2022-12-01 23:01:28 +0800

    update

--- p_save.php
+++ p_save.php
@@ -5,9 +5,9 @@ $sale = $_POST['sale'];
 $num = $_POST['num'];
 $unit = $_POST['unit'];
 
-echo "產品名稱" ,$name;
-echo "產品進價" ,$buy;
-echo "產品售價" ,$sale;
-echo "產品數量" ,$num;
-echo "產品單價" ,$unit;
+echo '產品名稱：',$name; echo '<br>';
+echo '產品進價：',$buy;  echo '<br>';
+echo '產品售價：',$sale; echo '<br>';
+echo '產品數量：',$num;  echo '<br>';
+echo '產品單價：',$unit; echo '<br>';
 ?>
