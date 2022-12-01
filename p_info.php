89ab2536f9cb1ad95509d0c0aeeafe1be22fc003
CSUMII\csumii <csumii1010@gmail.com>
2022-12-02 00:32:35 +0800

    update 1202

--- p_info.php
+++ p_info.php
@@ -12,71 +12,75 @@
     }
         </style>
 <?php
+session_start();
+header('Content-Type: text/html; charset=utf8');
+include_once("../../dbConnection.php");
+
+$db = new dbConnection();
+$SQLSTR= "SELECT p.name,n.resume_id,p.pid,p.zoneCode,left(n.create_date,10) from fproduct p, newinn n where p.pid=n.pid and (n.stage_id=11 or n.stage_id=12)  ";
+$result=$db->dbQuery($SQLSTR);
+
+$SQLSTR= "SELECT name FROM farm where farm_id=$farmID";
+$result=$db->dbQuery($SQLSTR);
+$rs=mysqli_fetch_row($result);
+$farmName=$rs[0];
+$SQLSTR= "SELECT DISTINCT Q.product_id, P.name ,F.name";
+$SQLSTR.=" FROM fproduct AS P LEFT JOIN QIBillDetail AS Q ON Q.product_id = P.pid LEFT JOIN farm AS F ON Q.farm_id = F.farm_id WHERE name='".$farmName."' order by p.name AND z zonecode ";
+$result=$db->dbQuery($SQLSTR);
+
 echo '<div class="mq"><marquee direction="left" scrollamount="7" behavior="alternate">
      我還沒有做完，所以很多功能還不能動~ 我有在認真練功 請不要罵我 我怕被罵 O//口//O </marquee></div>';
 
 echo '<center><h1>產品資訊</h1></center>';
 echo '<form action="p_save.php" method="post">
-<div class="topic">請輸入產品名稱：　<input type ="text" name="name" value=""> <br><br>
+<div class="topic">
+請輸入農場名稱：　<input type ="text" name="fname" value=""> <br><br>
+請輸入產品名稱：　<input type ="text" name="pname" value=""> <br><br>
 請輸入產品進價：　<input type ="num" name="buy" value="">   <br><br>
-請輸入產品售價：　<input type ="num" name="sale" value="">  <br><br>
+請輸入產品售價：　<input type ="num" name="price" value="">  <br><br>
 請輸入產品數量：　<input type ="num" name="num" value="">   <br><br>
-請輸入產品單價：　<input type ="text" name="unit" value=""> <br><br></div>
+請輸入產品單位：　<input type ="text" name="unit" value=""> <br><br></div>';
 
-<div class="topic">產品類別：
-<Select name="cate＂>
-<select>
-            <option>請選擇產品類別</option>
-            <!-- 不確定是否有資料表 -->
-            <optgroup label="食品">
-                <option>生鮮食品</option>
-                <option>冷凍食品</option>
-                <option>本土食品</option>
-                <option>健康食品</option>
-                <option>當季蔬果</option>
-                <option>烘培類</option>
-                <option>零食區</option>
-                <option>熟食</option>
-            </optgroup>       
+//產品類別
+$SQLSTR= "SELECT `sort_id`, `sort_name` FROM `market_sort`"; 
+$result=$db->dbQuery($SQLSTR);
+echo '<div class="topic">產品類別：
+<select name="sort" id="sort">>
+            <option value="0">請選擇產品類別</option>';
+            while($rs=mysqli_fetch_row($result)){
+                echo '<option value="' .$rs[0].'">' .$rs[1].'</option>';}
+            echo '</select></div> <br>';
 
-            <optgroup label="飲品">
-                <option>現打果汁</option>
-                <option>沖泡類</option>
-                <option>飲品</option>
-            </optgroup>
+//分類代號
+$SQLSTR= "SELECT `z_id`, `z_name` FROM `zonecode`" ;
+$result=$db->dbQuery($SQLSTR);
+echo '<div class="topic" id="zonecode">分類代號：
+        <select id="zonecode">
+        <option value="0">請選擇分類代號</option>';
+        while($rs=mysqli_fetch_row($result)){
+            echo '<option value="' .$rs[0].'">' .$rs[1].'</option>';}
+            echo '</select></div> <br>';
 
-            <optgroup label="兌換卷">
-                <option>商品抵用卷</option>
-                <option>餐飲抵用卷</option>
-            </optgroup>       
-            
-            <optgroup label="其它">
-                <option>茶醣類</option>
-                <option>加工品</option>
-                <option>日用品</option>
-                <option>精品區</option>                
-            </optgroup>  
-        </select>
-    </div> <br>
+//物品科別
+$SQLSTR= "SELECT `c-id`, `c-name` FROM `category`"; 
+$result=$db->dbQuery($SQLSTR);
+echo '<div class="topic">&nbsp&nbsp&nbsp&nbsp物品科別：
+        <select id="cname">
+        <option value="0">請選擇物品科別</option>';
+        while($rs=mysqli_fetch_row($result)){
+        echo '<option value="' .$rs[0].'">' .$rs[1].'</option>';}
+        echo '</select></div> <br>';
 
-    <div class="topic">分類代號：
-        <select>
-            <option>請選擇分類代號</option>
-            <!-- 待匯入資料表 -->
-        </select>
-    </div> <br>
+echo '<div class="topic">進貨日期： <input type="date" id="buydate" name="in-stock" 
+      value="0000-00-00" min="2022-01-01"> <br><br>
+      <div class="topic">到期日期： <input type="date" id="savedate" name="exp-stock"    value="0000-00-00" min="2022-01-01"> <br><br>';
+echo '<div class="topic">保存天數：<select id="deadline">
+      <option value="0">請選擇保存天數</option>';
+      while($rs=mysqli_fetch_row($result)){
+        echo '<option value="' .$rs[0].'">' .$rs[1].'</option>';}
+        echo '</select></div> <br>';
 
-    <div class="topic">物品科別：
-        <select>
-            <option>請選擇物品科別</option>
-            <!-- 待匯入資料表 -->
-        </select>　<br><br>
-
-    進貨日期： <input type="date" id="instockdate" name="in-stock" value="0000-00-00" min="2022-01-01"> <br><br>
-    到期日期： <input type="date" id="expdate" name="exp-stock"    value="0000-00-00" min="2022-01-01"> <br><br>
-    保存天數： <select><option>請選擇保存天數</option></select> <br><br>
-
-<input type ="submit" value="儲存產品資料">
+echo '<input type ="submit" value="儲存產品資料">
 </form>'
 ?>
 <script>
--- p_save.php
+++ p_save.php
@@ -13,80 +13,26 @@
         </style>
 <?php
 echo '<div class="mq"><marquee direction="left" scrollamount="7" behavior="alternate">
-     我還沒有做完，所以很多功能還不能動~ 我有在認真練功 請不要罵我 我怕被罵 O//口//O </marquee></div>';
+我還沒有做完，所以很多功能還不能動~ 我有在認真練功 請不要罵我 我怕被罵 O//口//O </marquee></div>';
 
-echo '<center><h1>產品資訊</h1></center>';
-echo '<form action="p_save.php" method="post">
-<div class="topic">請輸入產品名稱：　<input type ="text" name="name" value=""> <br><br>
-請輸入產品進價：　<input type ="num" name="buy" value="">   <br><br>
-請輸入產品售價：　<input type ="num" name="sale" value="">  <br><br>
-請輸入產品數量：　<input type ="num" name="num" value="">   <br><br>
-請輸入產品單價：　<input type ="text" name="unit" value=""> <br><br></div>
+echo '<center><h1>產品結帳</h1></center>';
 
-<div class="topic">產品類別：
-<Select name="cate＂>
-<select>
-            <option>請選擇產品類別</option>
-            <!-- 不確定是否有資料表 -->
-            <optgroup label="食品">
-                <option>生鮮食品</option>
-                <option>冷凍食品</option>
-                <option>本土食品</option>
-                <option>健康食品</option>
-                <option>當季蔬果</option>
-                <option>烘培類</option>
-                <option>零食區</option>
-                <option>熟食</option>
-            </optgroup>       
+$fname = $_POST['fname'];
+$pname = $_POST['pname'];
+$buydate = $_POST['buydate'];
+$sale = $_POST['price'];
+$num = $_POST['num'];
+$unit = $_POST['unit'];
+$cname = $_POST['cname'];
+$zonecode = $_POST['zonecode'];
+$pid = $str[0];  //產品編號  *取得產品名稱*
+$pid2=$_POST['pid2']; //已有的產品
 
-            <optgroup label="飲品">
-                <option>現打果汁</option>
-                <option>沖泡類</option>
-                <option>飲品</option>
-            </optgroup>
-
-            <optgroup label="兌換卷">
-                <option>商品抵用卷</option>
-                <option>餐飲抵用卷</option>
-            </optgroup>       
-            
-            <optgroup label="其它">
-                <option>茶醣類</option>
-                <option>加工品</option>
-                <option>日用品</option>
-                <option>精品區</option>                
-            </optgroup>  
-        </select>
-    </div> <br>
-
-    <div class="topic">分類代號：
-        <select>
-            <option>請選擇分類代號</option>
-            <!-- 待匯入資料表 -->
-        </select>
-    </div> <br>
-
-    <div class="topic">&nbsp&nbsp&nbsp&nbsp物品科別：
-        <select>
-            <option>請選擇物品科別</option>
-            <!-- 待匯入資料表 -->
-        </select>　<br><br>
-
-    進貨日期： <input type="date" id="instockdate" name="in-stock" value="0000-00-00" min="2022-01-01"> <br><br>
-    到期日期： <input type="date" id="expdate" name="exp-stock"    value="0000-00-00" min="2022-01-01"> <br><br>
-    保存天數： <select><option>請選擇保存天數</option></select> <br><br>
-
-<input type ="submit" value="儲存產品資料">
-</form>'
+echo '<div class="topic">農場名稱：',$fname; echo '<br><br>';
+echo '<div class="topic">產品名稱：',$pname; echo '<br><br>';
+echo '<div class="topic">產品進價：',$buydate;  echo '<br><br>';
+echo '<div class="topic">產品售價：',$price; echo '<br><br>';
+echo '<div class="topic">產品數量：',$num;  echo '<br><br>';
+echo '<div class="topic">產品單價：',$unit; echo '<br><br>';
+echo '<div class="topic">產品科別：',$cname; echo '<br><br>';
 ?>
-<script>
-        function process() {
-            const nameElement = document.getElementById('name');
-            const name = nameElement.value;
-            const buypriceElement = document.getElementById('buyprice');
-            const buyprice = buypriceElement.value;
-            const salepriceElement = document.getElementById('saleprice');
-            const saleprice = salepriceElement.value;
-            alert('產品品名：' + name + '\n產品進價：' + buyprice + '\n產品售價：' + saleprice);
-          }
-    </script>
