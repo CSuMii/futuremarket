ed6160b73ddefe4e3de31e823e6c047bb2867b8c
CSUMII\csumii <csumii1010@gmail.com>
2022-12-06 01:50:55 +0800

    update 1209

--- p_info.php
+++ p_info.php
@@ -10,7 +10,7 @@
         text-align: center;
         color: brown;
     }
-        </style>
+    </style>
 <?php
 session_start();
 header('Content-Type: text/html; charset=utf8');
@@ -20,36 +20,71 @@ $db = new dbConnection();
 $SQLSTR= "SELECT p.name,n.resume_id,p.pid,p.zoneCode,left(n.create_date,10) from fproduct p, newinn n where p.pid=n.pid and (n.stage_id=11 or n.stage_id=12)  ";
 $result=$db->dbQuery($SQLSTR);
 
-$SQLSTR= "SELECT name FROM farm where farm_id=$farmID";
+$farmID = @$_POST['farmID'];//農場編號
+$SQLSTR= "SELECT `farmname` FROM `farm` where `farm_id` =$farmID";
 $result=$db->dbQuery($SQLSTR);
-$rs=mysqli_fetch_row($result);
+@$rs=mysqli_fetch_row($result);
 $farmName=$rs[0];
-$SQLSTR= "SELECT DISTINCT Q.product_id, P.name ,F.name";
-$SQLSTR.=" FROM fproduct AS P LEFT JOIN QIBillDetail AS Q ON Q.product_id = P.pid LEFT JOIN farm AS F ON Q.farm_id = F.farm_id WHERE name='".$farmName."' order by p.name AND z zonecode ";
+$SQLSTR= "SELECT DISTINCT Q.product_id,P.name,F.name";
+$SQLSTR.=" FROM fproduct AS P LEFT JOIN QIBillDetail AS Q ON Q.product_id = P.pid LEFT JOIN farm AS F ON Q.farm_id = F.farm_id WHERE F.name ='".$farmName."' order by P.name";
 $result=$db->dbQuery($SQLSTR);
 
 echo '<div class="mq"><marquee direction="left" scrollamount="7" behavior="alternate">
      我還沒有做完，所以很多功能還不能動~ 我有在認真練功 請不要罵我 我怕被罵 O//口//O </marquee></div>';
 
+$fname = @$_GET['fname'];
+$farmID =@$_GET['farmID']; //取得農友的farm_id
+$db = new dbConnection();
+if($fname==""){
+    $_SESSION['farmID'] = $farmID;
+}else{	
+    $SQLSTR= "SELECT `farm_id`, `name` from `farm` where name='".$fname."'"; 
+    $result=$db->dbQuery($SQLSTR);
+    $rs=mysqli_fetch_row($result);
+    if($rs==""){
+        $fname = $_GET['fname'];
+        $SQLSTR= "INSERT INTO `farm`(`name`) VALUES ('".$fname."')";
+        $result=$db->dbQuery($SQLSTR);
+        $SQLSTR="SELECT MAX(farm_id) FROM `farm`";
+        $result=$db->dbQuery($SQLSTR);
+        $maxrol=mysqli_fetch_row($result);
+        $farmID=$maxrol[0];
+        $_SESSION['farmID'] = $farmID;
+    }else{
+        $fname=$rs[1];
+        $farmID=$rs[0];
+        $_SESSION['farmID'] = $farmID;
+    }
+}
 echo '<center><h1>產品資訊</h1></center>';
-echo '<form action="p_save.php" method="post">
-<div class="topic">
-請輸入農場名稱：　<input type ="text" name="fname" value=""> <br><br>
+echo '<form action="p_save.php" method="post">';
+
+//農場名稱
+$SQLSTR= "SELECT DISTINCT `farmname`,'farm-id'FROM `farm` ORDER BY 'farmname' ASC "; 
+$result=$db->dbQuery($SQLSTR);
+echo '<div class="topic">&nbsp&nbsp&nbsp&nbsp請選擇舊有的農場名稱：
+        <select name="selFARM">
+        <option value="0">請選擇農場名稱</option>';
+        while($rs=mysqli_fetch_row($result)){
+        echo '<option value="' .$rs[0].'">' .$rs[0].'</option>';}
+        echo '</select></div> <br>';
+
+echo '<div class="topic">請輸入新加入的農場名稱：<input type ="text" name="inputfname" value=""> <br><br>
 請輸入產品名稱：　<input type ="text" name="pname" value=""> <br><br>
-請輸入產品進價：　<input type ="num" name="buy" value="">   <br><br>
+請輸入產品進價：　<input type ="num" name="buydate" value="">   <br><br>
 請輸入產品售價：　<input type ="num" name="price" value="">  <br><br>
 請輸入產品數量：　<input type ="num" name="num" value="">   <br><br>
-請輸入產品單位：　<input type ="text" name="unit" value=""> <br><br></div>';
+請輸入產品單位：　<input type ="text" name="unit" value=""> <br><br></div>';    
 
 //產品類別
 $SQLSTR= "SELECT `sort_id`, `sort_name` FROM `market_sort`"; 
 $result=$db->dbQuery($SQLSTR);
 echo '<div class="topic">產品類別：
-<select name="sort" id="sort">>
+<select name="sort" id="sort">
             <option value="0">請選擇產品類別</option>';
             while($rs=mysqli_fetch_row($result)){
-                echo '<option value="' .$rs[0].'">' .$rs[1].'</option>';}
-            echo '</select></div> <br>';
+             echo '<option value="' .$rs[0].'">' .$rs[1].'</option>';}
+             echo '</select></div> <br>';
 
 //分類代號
 $SQLSTR= "SELECT `z_id`, `z_name` FROM `zonecode`" ;
@@ -65,15 +100,18 @@ echo '<div class="topic" id="zonecode">分類代號：
 $SQLSTR= "SELECT `c-id`, `c-name` FROM `category`"; 
 $result=$db->dbQuery($SQLSTR);
 echo '<div class="topic">&nbsp&nbsp&nbsp&nbsp物品科別：
-        <select id="cname">
+        <select name="cname">
         <option value="0">請選擇物品科別</option>';
         while($rs=mysqli_fetch_row($result)){
-        echo '<option value="' .$rs[0].'">' .$rs[1].'</option>';}
+        echo '<option value="' .$rs[1].'">' .$rs[1].'</option>';}
         echo '</select></div> <br>';
 
 echo '<div class="topic">進貨日期： <input type="date" id="buydate" name="in-stock" 
       value="0000-00-00" min="2022-01-01"> <br><br>
       <div class="topic">到期日期： <input type="date" id="savedate" name="exp-stock"    value="0000-00-00" min="2022-01-01"> <br><br>';
+
+$SQLSTR= "SELECT `s_id`, `s_name` FROM `storage`";
+$result=$db->dbQuery($SQLSTR);
 echo '<div class="topic">保存天數：<select id="deadline">
       <option value="0">請選擇保存天數</option>';
       while($rs=mysqli_fetch_row($result)){
@@ -82,15 +120,6 @@ echo '<div class="topic">保存天數：<select id="deadline">
 
 echo '<input type ="submit" value="儲存產品資料">
 </form>'
+
 ?>
-<script>
-        function process() {
-            const nameElement = document.getElementById('name');
-            const name = nameElement.value;
-            const buypriceElement = document.getElementById('buyprice');
-            const buyprice = buypriceElement.value;
-            const salepriceElement = document.getElementById('saleprice');
-            const saleprice = salepriceElement.value;
-            alert('產品品名：' + name + '\n產品進價：' + buyprice + '\n產品售價：' + saleprice);
-          }
-    </script>
+    
--- p_save.php
+++ p_save.php
@@ -12,21 +12,57 @@
     }
         </style>
 <?php
+include("../../dbConnection.php");
+date_default_timezone_set('Asia/Taipei');
+error_reporting(E_ALL & ~E_NOTICE);
 echo '<div class="mq"><marquee direction="left" scrollamount="7" behavior="alternate">
 我還沒有做完，所以很多功能還不能動~ 我有在認真練功 請不要罵我 我怕被罵 O//口//O </marquee></div>';
 
 echo '<center><h1>產品結帳</h1></center>';
 
-$fname = $_POST['fname'];
-$pname = $_POST['pname'];
-$buydate = $_POST['buydate'];
-$sale = $_POST['price'];
-$num = $_POST['num'];
-$unit = $_POST['unit'];
-$cname = $_POST['cname'];
-$zonecode = $_POST['zonecode'];
+$db = new dbConnection();
+$db->dbQuery("SET AUTOCOMMIT=0");
+$SQLSTR= "Insert into QIBill values('$billID','$billDate')";
+$result=$db->dbQuery($SQLSTR);
+
+$farmID = @$_POST['farmID'];//農場編號
+$farmname = $_POST['farmname'];//農場名稱
+$pname = $_POST['pname'];//產品名稱
 $pid = $str[0];  //產品編號  *取得產品名稱*
 $pid2=$_POST['pid2']; //已有的產品
+$buydate = $_POST['buydate'];//進貨價
+$price = $_POST['price'];//價格
+$num = $_POST['num'];//數量
+$unit = $_POST['unit']; //單位
+$cname = $_POST['cname'];//產品分類名稱
+$zonecode = $_POST['zonecode'];//產品分類代號
+
+$billID = substr(date("YmdHis", time()),0,14);
+$billDate = $billID;//substr($billID,0,8);
+$billID = "QI" . $billID;  //共16碼
+
+    for($i=0;$i<count($pro1)-1;$i++) 
+      {
+        $pro2 = explode('|', $pro1[$i]);
+        $num = explode('。', $pro2[0]);
+        $id = explode('。', $pro2[1]);
+        $SQLSTR= "UPDATE `stock` SET `stock`=stock-'$num[1]' where `product_id`='$id[2]' and `farm_id`='$id[0]' and `resume_id`='$id[1]'";
+        $result=$db->dbQuery($SQLSTR);
+      }   
+      for($i=0;$i<count($pro1)-1;$i++) //qibilldetails
+      {
+        $pro2 = explode('|', $pro1[$i]);
+        $num = explode('。', $pro2[0]);
+        $id = explode('。', $pro2[1]);
+        
+        $SQLSTR= "Insert into qibilldetail values('$billID','$od','$id[2]','$id[0]','$id[1]','$id[1]','$num[1]')";  
+        $result=$db->dbQuery($SQLSTR);
+        
+         $od++;
+      }
+
+$cname=$_POST['cname'];
+$fname=$_POST['selFARM'];
 
 echo '<div class="topic">農場名稱：',$fname; echo '<br><br>';
 echo '<div class="topic">產品名稱：',$pname; echo '<br><br>';
@@ -34,5 +70,5 @@ echo '<div class="topic">產品進價：',$buydate;  echo '<br><br>';
 echo '<div class="topic">產品售價：',$price; echo '<br><br>';
 echo '<div class="topic">產品數量：',$num;  echo '<br><br>';
 echo '<div class="topic">產品單價：',$unit; echo '<br><br>';
-echo '<div class="topic">產品科別：',$cname; echo '<br><br>';
+echo '<div class="topic">物品科別：',$cname; echo '<br><br>';
 ?>
