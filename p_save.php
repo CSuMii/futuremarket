4d6d6519f1a98fb6b90a38a0bfc3f8b3a8f90dff
chihleesumii <118653165+chihleesumii@users.noreply.github.com>
2022-12-07 11:41:59 +0800

    1207

--- p_save.php
+++ p_save.php
@@ -10,11 +10,17 @@
         text-align: center;
         color: brown;
     }
-        </style>
+    hyml,body{
+        background-color:#ABFFAB;
+    }
+</style>
 <?php
 include("../../dbConnection.php");
 date_default_timezone_set('Asia/Taipei');
 error_reporting(E_ALL & ~E_NOTICE);
+$timezone = date_default_timezone_get();
+date_default_timezone_set('Asia/Taipei');
+
 echo '<div class="mq"><marquee direction="left" scrollamount="7" behavior="alternate">
 我還沒有做完，所以很多功能還不能動~ 我有在認真練功 請不要罵我 我怕被罵 O//口//O </marquee></div>';
 
@@ -30,12 +36,19 @@ $farmname = $_POST['farmname'];//農場名稱
 $pname = $_POST['pname'];//產品名稱
 $pid = $str[0];  //產品編號  *取得產品名稱*
 $pid2=$_POST['pid2']; //已有的產品
-$buydate = $_POST['buydate'];//進貨價
 $price = $_POST['price'];//價格
 $num = $_POST['num'];//數量
 $unit = $_POST['unit']; //單位
 $cname = $_POST['cname'];//產品分類名稱
 $zonecode = $_POST['zonecode'];//產品分類代號
+$cname=$_POST['cname']; //產品分類
+$sort=$_POST['sort']; //產品類別
+$buyprice=$_POST['buyprice']; //產品進價
+$fname=$_POST['selFARM']; 
+$buydate=$_POST['buydate'];
+$zonecode=$_POST['zonecode'];
+$storagedate=$_POST['storagedate'];
+$deadline=$_POST['deadline'];
 
 $billID = substr(date("YmdHis", time()),0,14);
 $billDate = $billID;//substr($billID,0,8);
@@ -61,14 +74,17 @@ $billID = "QI" . $billID;  //共16碼
          $od++;
       }
 
-$cname=$_POST['cname'];
-$fname=$_POST['selFARM'];
-
 echo '<div class="topic">農場名稱：',$fname; echo '<br><br>';
 echo '<div class="topic">產品名稱：',$pname; echo '<br><br>';
-echo '<div class="topic">產品進價：',$buydate;  echo '<br><br>';
+echo '<div class="topic">產品進價：',$buyprice;  echo '<br><br>';
 echo '<div class="topic">產品售價：',$price; echo '<br><br>';
 echo '<div class="topic">產品數量：',$num;  echo '<br><br>';
 echo '<div class="topic">產品單價：',$unit; echo '<br><br>';
+echo '<div class="topic">產品類別：',$sort; echo '<br><br>';
+echo '<div class="topic">產品代號：',$zonecode; echo '<br><br>';
 echo '<div class="topic">物品科別：',$cname; echo '<br><br>';
+echo '<div class="topic">進貨日期：',$buydate; echo '<br><br>';
+echo '<div class="topic">到期日期：',$storagedate; echo '<br><br>';
+echo '<div class="topic">保存天數：',$deadline; echo '<br><br>';
+
 ?>
